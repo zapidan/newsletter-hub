020efaebe34b9556d8c11785ac4336f087547da2
zapidan <zapidan@gmail.com>
2025-06-24 00:08:43 -0500

    more test fixes

--- src/common/api/newsletterApi.ts
+++ src/common/api/newsletterApi.ts
@@ -84,7 +84,7 @@ const transformNewsletterResponse = (data: any): NewsletterWithRelations => {
       .filter((tag: Tag | null): tag is Tag => tag !== null)
     : [];
 
-  const { _newsletter_sources, source: rawSource, tags: rawTags, ...restOfData } = data;
+  const { _newsletter_sources, source: _rawSource, tags: _rawTags, ...restOfData } = data;
 
   const result: NewsletterWithRelations = {
     // Include all base Newsletter properties first
